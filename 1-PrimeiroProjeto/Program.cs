018068c5fef21f44e4fb3a46bd0d4a5d9a9f002b
Ana Figueira <anafigueira@MacBook-Pro-de-Ana.local>
2021-11-07 15:39:40 -0300

    Initial commit

---
+++ 1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("A execução acabou. Aperte Enter para visualizar.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
