03d09ad92d0ff5b47cfcb0758bcfb8a8bf84f4c5
NaluFigueira <anachfigueira@gmail.com>
2021-11-07 18:25:03 -0300

    Repetition

---
+++ Fatorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int fatorial = 1;
+            for (int n = 1; n <= 10; n++)
+            {
+                fatorial *= n;
+
+                Console.WriteLine("O fatorial de " + n + " é " + fatorial);
+            }
+        }
+    }
+}
---
+++ Multiplos3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Multiplos3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for(int multiplo = 3; multiplo < 100; multiplo+=3)
+            {
+                Console.WriteLine(multiplo);
+            }
+        }
+    }
+}
