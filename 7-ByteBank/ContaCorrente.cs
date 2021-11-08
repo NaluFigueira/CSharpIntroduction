cc15df570e2995987b6ab39c26061914ce159f7a
NaluFigueira <anachfigueira@gmail.com>
2021-11-07 23:46:41 -0300

    Introduction to classes

---
+++ 7-ByteBank/ContaCorrente.cs
@@ -0,0 +1,33 @@
+﻿public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo >= valor)
+        {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir (double valor, ContaCorrente contaDestino)
+    {
+        if(this.saldo >= valor)
+        {
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+        return false;
+    }
+};
---
+++ 7-ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Criou conta corrente.");
+            ContaCorrente primeiraContaCorrente = new ContaCorrente();
+
+            primeiraContaCorrente.saldo = 200;
+
+            Console.WriteLine("O saldo é R$ " + primeiraContaCorrente.saldo + ",00.");
+
+            primeiraContaCorrente.saldo += 100;
+
+            Console.WriteLine("Crédito de R$ 100, 00.");
+            Console.WriteLine("O saldo é R$ " + primeiraContaCorrente.saldo + ",00.");
+
+            Console.WriteLine("Criou conta corrente.");
+            ContaCorrente segundaContaCorrente = new ContaCorrente();
+
+            segundaContaCorrente.saldo = 50;
+
+            Console.WriteLine("O saldo da primeira conta é R$ " + primeiraContaCorrente.saldo + ",00.");
+            Console.WriteLine("O saldo da segunda conta é R$ " + segundaContaCorrente.saldo + ",00.");
+
+        }
+    }
+}
