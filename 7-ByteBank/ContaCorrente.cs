b048963eb53144003574520215486e768e67f73f
NaluFigueira <anachfigueira@gmail.com>
2021-11-12 17:19:30 -0300

    Getters and setters

--- 7-ByteBank/ContaCorrente.cs
+++ 7-ByteBank/ContaCorrente.cs
@@ -1,17 +1,34 @@
 ﻿namespace ByteBank
-{ 
+{
     public class ContaCorrente
     {
-        public Cliente titular;
-        public int agencia;
-        public int numero;
-        public double saldo;
+        public Cliente Titular { get; set; }
+
+        public int Agencia { get; set; }
+
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value > 0)
+                    _saldo = value;
+                return;
+            }
+        }
 
         public bool Sacar(double valor)
         {
-            if (this.saldo >= valor)
+            if (_saldo >= valor)
             {
-                this.saldo -= valor;
+                _saldo -= valor;
                 return true;
             }
             return false;
@@ -19,18 +36,20 @@
 
         public void Depositar(double valor)
         {
-            this.saldo += valor;
+            _saldo += valor;
         }
 
-        public bool Transferir (double valor, ContaCorrente contaDestino)
+        public bool Transferir(double valor, ContaCorrente contaDestino)
         {
-            if(this.saldo >= valor)
+            if (_saldo >= valor)
             {
-                this.saldo -= valor;
+                _saldo -= valor;
                 contaDestino.Depositar(valor);
                 return true;
             }
             return false;
         }
+
+
     };
 }
--- 7-ByteBank/Program.cs
+++ 7-ByteBank/Program.cs
@@ -6,32 +6,25 @@ namespace ByteBank
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Criou conta corrente.");
+            Console.WriteLine("Criando conta corrente...");
             ContaCorrente primeiraContaCorrente = new ContaCorrente();
+            primeiraContaCorrente.Saldo = 200;
+            Console.WriteLine("O saldo atual é R$ " + primeiraContaCorrente.Saldo + ",00.");
 
-            primeiraContaCorrente.saldo = 200;
+            Console.WriteLine("Sacando R$ 100,00...");
+            primeiraContaCorrente.Sacar(100);
+            Console.WriteLine("O saldo atual é R$ " + primeiraContaCorrente.Saldo + ",00.");
 
-            Console.WriteLine("O saldo é R$ " + primeiraContaCorrente.saldo + ",00.");
-
-            primeiraContaCorrente.saldo += 100;
-
-            Console.WriteLine("Crédito de R$ 100, 00.");
-            Console.WriteLine("O saldo é R$ " + primeiraContaCorrente.saldo + ",00.");
-
-            Console.WriteLine("Criou conta corrente.");
+            Console.WriteLine("Criando outra conta corrente...");
             ContaCorrente segundaContaCorrente = new ContaCorrente();
+            segundaContaCorrente.Saldo = 50;
+            Console.WriteLine("O saldo atual da primeira conta é R$ " + primeiraContaCorrente.Saldo + ",00.");
+            Console.WriteLine("O saldo atual da segunda conta é R$ " + segundaContaCorrente.Saldo + ",00.");
 
-            segundaContaCorrente.saldo = 50;
-
-            Console.WriteLine("O saldo da primeira conta é R$ " + primeiraContaCorrente.saldo + ",00.");
-            Console.WriteLine("O saldo da segunda conta é R$ " + segundaContaCorrente.saldo + ",00.");
-
-            Console.WriteLine("Adicionando titular a primeira conta.");
-
-            primeiraContaCorrente.titular = new Cliente();
-            primeiraContaCorrente.titular.nome = "Camila";
-
-            Console.WriteLine("A titular da primeira conta é a " + primeiraContaCorrente.titular.nome);
+            Console.WriteLine("Adicionando titular na primeira conta...");
+            primeiraContaCorrente.Titular = new Cliente();
+            primeiraContaCorrente.Titular.Nome = "Camila";
+            Console.WriteLine("A titular da primeira conta é a " + primeiraContaCorrente.Titular.Nome + ".");
         }
     }
 }
