8198eb114f3e916d59dd51c2a68c7c89e7657d9b
NaluFigueira <anachfigueira@gmail.com>
2021-11-23 18:35:48 -0300

    DLL reference

---
+++ ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaInterno
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(1, 1);
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
