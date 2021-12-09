7f4aea14ce3549068db83538a773a5aad4d3bd39
NaluFigueira <anachfigueira@gmail.com>
2021-12-08 21:53:46 -0300

    Object class

--- ByteBank.Modelos/Cliente.cs
+++ ByteBank.Modelos/Cliente.cs
@@ -11,5 +11,20 @@ namespace ByteBank.Modelos
         public string Nome { get; set; }
         public string CPF { get; set; }
         public string Profissao { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            try
+            {
+                Cliente cliente = obj as Cliente;
+
+                return CPF == cliente.CPF;
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("O objeto comparado não é um cliente.");
+                return false;
+            }
+        }
     }
 }
