7334e335567d330d8a6accadd8452f9fe9b2c28f
NaluFigueira <anachfigueira@gmail.com>
2021-12-10 14:35:26 -0300

    Creating list

---
+++ ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+
+    public class ListaDeContaCorrente
+    {
+        private ContaCorrente[] _contas;
+        public int _proximaPosicao;
+
+        public ListaDeContaCorrente(int tamanhoInicial = 5)
+        {
+            _contas = new ContaCorrente[tamanhoInicial];
+            _proximaPosicao = 0;
+        }
+
+        public void Adicionar(ContaCorrente conta)
+        {
+            VerificaCapacidade(_proximaPosicao + 1);
+
+            _contas[_proximaPosicao] = conta;
+            _proximaPosicao++;
+        }
+
+        private void VerificaCapacidade(int tamanhoNecessario)
+        {
+            if (_contas.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            ContaCorrente[] novoArray =
+                new ContaCorrente[tamanhoNecessario];
+
+            for (int indice = 0; indice < _contas.Length; indice++)
+            {
+                novoArray[indice] = _contas[indice];
+            }
+
+            Array.Copy(
+                sourceArray: _contas,
+                destinationArray: novoArray,
+                length: _contas.Length
+            );
+
+            _contas = novoArray;
+        }
+    }
+}
--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -8,19 +8,16 @@ namespace ByteBank.SistemaAgencia
     {
         public static void Main(string[] args)
         {
-            ContaCorrente[] contas = new ContaCorrente[]
-            {
-                new ContaCorrente(874, 1234567),
-                new ContaCorrente(874, 1234567),
-                new ContaCorrente(874, 1234567),
-                new ContaCorrente(874, 1234567),
-            };
 
-            for(int indice = 0; indice < contas.Length; indice++)
-            {
-                ContaCorrente contaAtual = contas[indice];
-                Console.WriteLine($"Conta {indice + 1}: {contaAtual}");
-            }
+            ListaDeContaCorrente contas = new ListaDeContaCorrente();
+
+            contas.Adicionar(new ContaCorrente(874, 1234567));
+            contas.Adicionar(new ContaCorrente(874, 1234567));
+            contas.Adicionar(new ContaCorrente(874, 1234567));
+            contas.Adicionar(new ContaCorrente(874, 1234567));
+            contas.Adicionar(new ContaCorrente(874, 1234567));
+            contas.Adicionar(new ContaCorrente(874, 1234567));
+            contas.Adicionar(new ContaCorrente(874, 1234567));
         }
     }
 }
