977c1eb3fb9b51da45e67164ad05311aade3e533
NaluFigueira <anachfigueira@gmail.com>
2021-12-14 19:45:29 -0300

    StreamReader

---
+++ ByteBank.ImportacaoExportacao/LidandoComStreamDiretamente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ByteBank.ImportacaoExportacao
+{
+    partial class MainClass
+    {
+        
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = Encoding.UTF8;
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+
+            Console.WriteLine(texto);
+        }
+
+        static void LidandoComFileStreamDiretamente()
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024];
+
+                var numeroDeBytesLidos = -1;
+
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+            }
+        }
+    }
+}
--- ByteBank.ImportacaoExportacao/Program.cs
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -4,32 +4,24 @@ using System.Text;
 
 namespace ByteBank.ImportacaoExportacao
 {
-    class MainClass
+    partial class MainClass
     {
-        static void EscreverBuffer(byte[] buffer)
-        {
-            var utf8 = Encoding.UTF8;
-
-            var texto = utf8.GetString(buffer);
-
-            Console.WriteLine(texto);
-        }
 
         public static void Main(string[] args)
         {
+
             var enderecoDoArquivo = "contas.txt";
 
-            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-            var buffer = new byte[1024];
-
-            var numeroDeBytesLidos = -1;
-
-
-            while(numeroDeBytesLidos != 0)
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDoArquivo))
             {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
+
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+
+                    Console.WriteLine(linha);
+                }
             }
         }
     }
