4deb9fc7bd983a8b39013253c0ed18127138e9a0
NaluFigueira <anachfigueira@gmail.com>
2021-12-17 01:06:56 -0300

    Console Stream

--- ByteBank.ImportacaoExportacao/Program.cs
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -7,80 +7,35 @@ namespace ByteBank.ImportacaoExportacao
 {
     partial class MainClass
     {
-        static void CriarArquivo()
+        static void UsarStreamEntrada()
         {
-            var caminhoNovoArquivo = "contasExportadas.csv";
-
-            using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var fluxoDeEntrada = Console.OpenStandardInput())
+            using (var fs = new FileStream("entradaConsole.txt", FileMode.Create))
             {
-                var contaComoString = "456,78495,4785.50,Gustavo Santos";
-                var enconding = Encoding.UTF8;
-                var bytes = enconding.GetBytes(contaComoString);
+                var buffer = new byte[1024];
 
-                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
-            }
-        }
-
-        static void CriarArquivoComWriter()
-        {
-            var caminhoNovoArquivo = "contasExportadasWriter.csv";
-
-            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
-            using (var escritor = new StreamWriter(fluxoDeArquivo))
-            {
-                escritor.Write("456,78495,4785.50,Gustavo Santos");
-            }
-        }
-
-        static void Flush()
-        {
-            var caminhoNovoArquivo = "teste.txt";
-
-            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
-            using (var escritor = new StreamWriter(fluxoDeArquivo))
-            {
-                for (int i = 0; i < 1000000000; i++)
+                while (true)
                 {
-                    escritor.WriteLine($"Linha {i}");
-                    escritor.Flush(); // Despeja o buffer para o Stream
 
-                    Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter p adicionar mais uma!");
-                    Console.ReadLine();
+                    var bytesLidos = fluxoDeEntrada.Read(buffer, 0, 1024);
+
+                    fs.Write(buffer, 0, bytesLidos);
+
+                    fs.Flush();
                 }
-            }
-        }
-
-        static void EscritaBinaria()
-        {
-            var caminhoNovoArquivo = "escritaBinaria.txt";
-            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
-            using (var escritor = new BinaryWriter(fluxoDeArquivo))
-            {
-                escritor.Write(456);
-                escritor.Write(546544);
-                escritor.Write(4050.50);
-                escritor.Write("Gustavo Braga");
-            }
-        }
-
-        static void LeituraBinaria()
-        {
-            using (var fluxoDeArquivo = new FileStream("escritaBinaria.txt", FileMode.Open))
-            using (var leitor = new BinaryReader(fluxoDeArquivo))
-            {
-                var agencia = leitor.ReadInt32();
-                var numeroConta = leitor.ReadInt32();
-                var saldo = leitor.ReadDouble();
-                var titular = leitor.ReadString();
-
-                Console.WriteLine($"{agencia}/{numeroConta} {titular} {saldo}");
             }
         }
 
         public static void Main(string[] args)
         {
-            EscritaBinaria();
-            LeituraBinaria();
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+            Console.WriteLine("Arquivo escrevendoComAClasseFile.txt criado!");
+
+            var bytesArquivo = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
         }
     }
 }
---
+++ ByteBank.ImportacaoExportacao/UsandoBinaryWriter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ByteBank.ImportacaoExportacao
+{
+    partial class MainClass
+    {
+
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "456,78495,4785.50,Gustavo Santos";
+                var enconding = Encoding.UTF8;
+                var bytes = enconding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        static void CriarArquivoComWriter()
+        {
+            var caminhoNovoArquivo = "contasExportadasWriter.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                escritor.Write("456,78495,4785.50,Gustavo Santos");
+            }
+        }
+
+        static void Flush()
+        {
+            var caminhoNovoArquivo = "teste.txt";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    escritor.WriteLine($"Linha {i}");
+                    escritor.Flush(); // Despeja o buffer para o Stream
+
+                    Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter p adicionar mais uma!");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        static void EscritaBinaria()
+        {
+            var caminhoNovoArquivo = "escritaBinaria.txt";
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new BinaryWriter(fluxoDeArquivo))
+            {
+                escritor.Write(456);
+                escritor.Write(546544);
+                escritor.Write(4050.50);
+                escritor.Write("Gustavo Braga");
+            }
+        }
+
+        static void LeituraBinaria()
+        {
+            using (var fluxoDeArquivo = new FileStream("escritaBinaria.txt", FileMode.Open))
+            using (var leitor = new BinaryReader(fluxoDeArquivo))
+            {
+                var agencia = leitor.ReadInt32();
+                var numeroConta = leitor.ReadInt32();
+                var saldo = leitor.ReadDouble();
+                var titular = leitor.ReadString();
+
+                Console.WriteLine($"{agencia}/{numeroConta} {titular} {saldo}");
+            }
+        }
+    }
+}
