f2252410ad001eec2a34735b01a3bd9a9433f723
NaluFigueira <anachfigueira@gmail.com>
2021-12-17 00:50:18 -0300

    Flush and binary files

--- ByteBank.ImportacaoExportacao/Program.cs
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -32,9 +32,55 @@ namespace ByteBank.ImportacaoExportacao
             }
         }
 
+        static void Flush()
+        {
+            var caminhoNovoArquivo = "teste.txt";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    escritor.WriteLine($"Linha {i}");
+                    escritor.Flush(); // Despeja o buffer para o Stream
+
+                    Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter p adicionar mais uma!");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        static void EscritaBinaria()
+        {
+            var caminhoNovoArquivo = "escritaBinaria.txt";
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new BinaryWriter(fluxoDeArquivo))
+            {
+                escritor.Write(456);
+                escritor.Write(546544);
+                escritor.Write(4050.50);
+                escritor.Write("Gustavo Braga");
+            }
+        }
+
+        static void LeituraBinaria()
+        {
+            using (var fluxoDeArquivo = new FileStream("escritaBinaria.txt", FileMode.Open))
+            using (var leitor = new BinaryReader(fluxoDeArquivo))
+            {
+                var agencia = leitor.ReadInt32();
+                var numeroConta = leitor.ReadInt32();
+                var saldo = leitor.ReadDouble();
+                var titular = leitor.ReadString();
+
+                Console.WriteLine($"{agencia}/{numeroConta} {titular} {saldo}");
+            }
+        }
+
         public static void Main(string[] args)
         {
-            CriarArquivoComWriter();
+            EscritaBinaria();
+            LeituraBinaria();
         }
     }
 }
